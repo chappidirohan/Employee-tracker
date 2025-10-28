d88ffa8c1ede7c2754313ea8dc7ae518dabfe33d
ManasaYeturu <manasa16nbkr@gmail.com>
2025-10-28 12:54:07 +0530

    i have added GIT line in main method.

--- demo (7)/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo (7)/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
         System.out.println("hi");
 		System.out.println("hello");
+		System.out.println("GIT");
 	}
 
 }
